f274fe321ddf12207d79873bc2195a55b3e57892
realmayus <realmayus@gmail.com>
2023-11-22 13:09:57 +0100

    Fix examples/io.rs

--- examples/io.rs
+++ examples/io.rs
@@ -5,14 +5,15 @@ use async_nbt::io::{read_nbt, write_nbt, Flavor, NbtIoError};
 const UNCOMPRESSED_NBT: &[u8] = include_bytes!("./assets/uncompressed_nbt.nbt");
 const GZ_COMPRESSED_NBT: &[u8] = include_bytes!("./assets/gz_compressed_nbt.nbt");
 
-fn main() -> Result<(), NbtIoError> {
+#[tokio::main]
+async fn main() -> Result<(), NbtIoError> {
     // You can read nbt from raw bytes
-    let nbt = read_nbt(&mut Cursor::new(UNCOMPRESSED_NBT), Flavor::Uncompressed, false)?.0;
+    let nbt = read_nbt(&mut Cursor::new(UNCOMPRESSED_NBT), Flavor::Uncompressed, false).await?.0;
 
     println!("uncompressed: {}", nbt);
 
     // by changing the Flavor you can read compressed nbt too
-    let compressed_nbt = read_nbt(&mut Cursor::new(GZ_COMPRESSED_NBT), Flavor::GzCompressed, false)?.0;
+    let compressed_nbt = read_nbt(&mut Cursor::new(GZ_COMPRESSED_NBT), Flavor::GzCompressed, false).await?.0;
 
     println!("gz compressed: {}", compressed_nbt);
 
